b92d2b4d7d64d50c543ff85a46463c52e787b54b
Daniel Gooden <dan.gooden.dev@gmail.com>
2024-05-30 02:32:59 +1000

    finalizing a good nvim workflow

--- lua/squid/packer.lua
+++ lua/squid/packer.lua
@@ -30,6 +30,16 @@ return require('packer').startup(function(use)
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
     use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+            { "MunifTanjim/nui.nvim" },
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    }
+    use('nvim-tree/nvim-web-devicons')
+    use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
@@ -41,16 +51,6 @@ return require('packer').startup(function(use)
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'L3MON4D3/LuaSnip' },
-        }
-    }
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     }
 end)
